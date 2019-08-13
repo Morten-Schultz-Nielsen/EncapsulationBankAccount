2e3e906ae7a67963e00c62c7ae71a2621ed18a24
mort358d <mort358d@CV-BB-5860>
2019-08-13 09:08:48 +0200

    Started project

---
+++ EncapsulationBankAccount.Entities.Tests/AccountTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EncapsulationBankAccount.Entities;
+
+namespace EncapsulationBankAccount.Entities.Tests
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void TestAccount()
+        {
+
+        }
+    }
+}
---
+++ EncapsulationBankAccount.Entities.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EncapsulationBankAccount.Entities.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EncapsulationBankAccount.Entities.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3432cd29-3ac3-4a6f-955f-25497bba916b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
